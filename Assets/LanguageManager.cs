a75f7c79e143206a165df73b5283d88f47133bf3
Sergei <surgery_333@mail.ru>
2024-11-22 08:52:03 +0300

    Added yandex SDK scripting for auto-localization in browser and some new script for it

---
+++ Assets/LanguageManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class LanguageManager : MonoBehaviour {
+
+    [DllImport("__Internal")]
+    private static extern string GetLang();
+
+    // ru en
+    public string CurrentLanguage;
+
+    public static LanguageManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/LocalizationScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizationScript : MonoBehaviour {
+	[SerializeField] string _ru;
+	[SerializeField] string _en;
+	
+	void Start () {
+		if (LanguageManager.Instance.CurrentLanguage == "ru")
+		{
+			GetComponent<Text>().text = _ru;
+		}
+		else if (LanguageManager.Instance.CurrentLanguage == "en")
+		{
+			GetComponent<Text>().text = _en;
+		}
+		else
+		{
+			GetComponent<Text>().text = _en;
+		}
+	}
+}
